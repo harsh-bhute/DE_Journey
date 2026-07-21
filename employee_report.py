941bc5a2a9858ea9ae031d451d1b1cb0de82dede
Harsh <hbhute28@gmail.com>
2026-07-21 19:34:31 +0530

    Added comments to the week one files

--- employee_report.py
+++ employee_report.py
@@ -3,6 +3,7 @@ import sqlite3
 conn = sqlite3.connect("report.db")
 cursor = conn.cursor()
 
+# Creation of Table
 cursor.execute(
     """CREATE TABLE IF NOT EXISTS EMPLOYEES(
         ID INT,
@@ -15,6 +16,8 @@ cursor.execute(
 conn.commit()
 print("Table Created")
 
+# Addition of Data to table
+
 cursor.execute("INSERT INTO EMPLOYEES VALUES(1,'HARSH','IT')")
 cursor.execute("INSERT INTO EMPLOYEES VALUES(2,'KARAN','IT')")
 cursor.execute("INSERT INTO EMPLOYEES VALUES(3,'DEVESH','SALES')")
@@ -24,14 +27,14 @@ cursor.execute("INSERT INTO EMPLOYEES VALUES(6,'PAWAN','HR')")
 cursor.execute("INSERT INTO EMPLOYEES VALUES(7,'NAVIN','SALES')")
 
 conn.commit()
-
+# getting all users 
 cursor.execute("SELECT * FROM EMPLOYEES")
 ROWS = cursor.fetchall()
 
 for row in ROWS:
     print(f"ID:{row[0]} | Name:{row[1]} | Dept:{row[2]}")
 
-
+# Function to get the employees by their Dept.
 def get_employees_by_dept(dept):
     
     cursor.execute("SELECT * FROM EMPLOYEES WHERE DEPT = ?",(dept,))
